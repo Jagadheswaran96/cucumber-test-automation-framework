8c31d000fce1efc52b3542bf880e0bebe3f1d5a2
Jagadheswaran <jagadhesjack88@gmail.com>
2026-02-04 02:35:23 +0530

    Screenshot utils updated

--- src/main/java/com/lambda/cucumber/utils/ScreenshotUtils.java
+++ src/main/java/com/lambda/cucumber/utils/ScreenshotUtils.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class ScreenshotUtils {
-
-    public static byte[] getScreenshot(){
-        return ((TakesScreenshot) DriverFactory.getDriver())
-                .getScreenshotAs(OutputType.BYTES);
+    
+    public static byte[] getScreenshot() { 
+    	if (DriverFactory.getDriver() == null) { 
+    		return new byte[0]; // return empty screenshot 
+    		} 
+    	return ((TakesScreenshot) DriverFactory.getDriver()) .getScreenshotAs(OutputType.BYTES);
     }
 }
